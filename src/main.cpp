9afa1a1e3ffef6acb030bf87d4573b89602a00e8
juice-artur <asasin.lol10@gmail.com>
2024-08-29 21:51:18 +0300

    feat: flip image

--- src/main.cpp
+++ src/main.cpp
@@ -77,7 +77,7 @@ int main()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	int width, height, nrChannels;
-
+	stbi_set_flip_vertically_on_load(true);
 	unsigned char* data = stbi_load("../res/wooden_container.jpg", &width, &height, &nrChannels, 0);
 	if (data)
 	{
