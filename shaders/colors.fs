9d24ac33fdbbbf2a02a8bb0a7e55d86d666c5fa6
juice-artur <59645676+juice-artur@users.noreply.github.com>
2024-09-05 21:32:59 +0300

    feat: implement point lighting (#19)

--- shaders/colors.fs
+++ shaders/colors.fs
@@ -3,16 +3,20 @@ out vec4 FragColor;
 
 struct Material {
     sampler2D diffuse;
-    sampler2D specular;   
+    sampler2D specular;    
     float shininess;
 }; 
 
 struct Light {
-    vec3 position;
-
+    vec3 position;  
+  
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
+	
+    float constant;
+    float linear;
+    float quadratic;
 };
 
 in vec3 FragPos;  
@@ -35,7 +39,14 @@ void main()
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
+    vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;  
+    
+    float distance = length(light.position - FragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));    
+
+    ambient *= attenuation;  
+    diffuse *= attenuation;
+    specular *= attenuation;   
         
     vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, 1.0);
