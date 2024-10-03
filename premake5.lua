1b0f8569e8da3ca4c371a526e11e63e7c592a135
juice-artur <asasin.lol10@gmail.com>
2024-10-04 00:40:46 +0300

    feat: add premake

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "JuicyEngine"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "JuicyEngine"
+	location "JuicyEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"JE_PLATFORM_WINDOWS",
+			"JE_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "JE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "JE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "JE_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"JuicyEngine/vendor/spdlog/include",
+		"JuicyEngine/src"
+	}
+
+	links
+	{
+		"JuicyEngine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"JE_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "JE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "JE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "JE_DIST"
+		optimize "On"
