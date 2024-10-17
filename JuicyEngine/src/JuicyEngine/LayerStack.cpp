8ca6968190a21a5a53e31f1f6042427697c6ba88
juice-artur <asasin.lol10@gmail.com>
2024-10-17 15:12:51 +0300

    fix: fix detaching layers

--- JuicyEngine/src/JuicyEngine/LayerStack.cpp
+++ JuicyEngine/src/JuicyEngine/LayerStack.cpp
@@ -19,16 +19,21 @@ void LayerStack::PushOverlay(Layer* overlay)
 }
 void LayerStack::PopLayer(Layer* layer)
 {
-    auto it = std::find(m_Layers.begin(), m_Layers.end(), layer);
+    auto it = std::find(m_Layers.begin(), m_Layers.begin() + m_LayerInsertIndex, layer);
     if (it != m_Layers.end())
     {
+        layer->OnDetach();
         m_Layers.erase(it);
         m_LayerInsertIndex--;
     }
 }
 void LayerStack::PopOverlay(Layer* overlay)
 {
-    auto it = std::find(m_Layers.begin(), m_Layers.end(), overlay);
-    if (it != m_Layers.end()) m_Layers.erase(it);
+    auto it = std::find(m_Layers.begin() + m_LayerInsertIndex, m_Layers.end(), overlay);
+    if (it != m_Layers.end())
+    {
+        overlay->OnDetach();
+        m_Layers.erase(it);
+    }
 }
 }  // namespace JuicyEngine
