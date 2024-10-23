75778e9477c46de4495c84ed74c3b2164c0ed4e5
juice-artur <asasin.lol10@gmail.com>
2024-10-24 00:50:03 +0300

    feat: add alias for smart pointer by using on front-end

--- JuicyEngine/src/JuicyEngine/Core.h
+++ JuicyEngine/src/JuicyEngine/Core.h
@@ -1,4 +1,7 @@
 #pragma once
+
+#include <memory>
+
 #ifdef JE_PLATFORM_WINDOWS
 #if JE_DYNAMIC_LINK
 #ifdef JE_BUILD_DLL
@@ -42,3 +45,11 @@
 #define BIT(x) (1 << x)
 
 #define JE_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+
+namespace JuicyEngine
+{
+template <typename T>
+using Scope = std::unique_ptr<T>;
+template <typename T>
+using Ref = std::shared_ptr<T>;
+}  // namespace JuicyEngine
