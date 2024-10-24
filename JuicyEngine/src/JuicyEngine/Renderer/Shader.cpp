c9465630597ebcc21f81f76f824bab18edf459c4
juice-artur <asasin.lol10@gmail.com>
2024-10-24 21:10:06 +0300

    feat: read shaders from file

--- JuicyEngine/src/JuicyEngine/Renderer/Shader.cpp
+++ JuicyEngine/src/JuicyEngine/Renderer/Shader.cpp
@@ -6,6 +6,16 @@
 
 namespace JuicyEngine
 {
+Shader* Shader::Create(const std::string& filepath)
+{
+    switch (Renderer::GetAPI())
+    {
+        case RendererAPI::API::None: JE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+        case RendererAPI::API::OpenGL: return new OpenGLShader(filepath);
+    }
+    JE_CORE_ASSERT(false, "Unknown RendererAPI!");
+    return nullptr;
+}
 
 Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc)
 {
