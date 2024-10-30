da2566b3dae913d3bcd4e2bd2070d797ea9ce195
juice-artur <asasin.lol10@gmail.com>
2024-10-30 17:50:10 +0200

    feat: implement resize window event

--- JuicyEngine/src/JuicyEngine/Application.h
+++ JuicyEngine/src/JuicyEngine/Application.h
@@ -23,6 +23,7 @@ public:
 
 private:
     bool OnWindowClose(WindowCloseEvent& e);
+    bool OnWindowResize(WindowResizeEvent& e);
 
 private:
     std::unique_ptr<Window> m_Window;
@@ -30,6 +31,7 @@ private:
     bool m_Running = true;
     LayerStack m_LayerStack;
     float m_LastFrameTime = 0.0f;
+    bool m_Minimized = false;
 
 private:
     static Application* s_Instance;
--- JuicyEngine/src/JuicyEngine/Renderer/Renderer.h
+++ JuicyEngine/src/JuicyEngine/Renderer/Renderer.h
@@ -11,6 +11,7 @@ class Renderer
 {
 public:
     static void Init();
+    static void OnWindowResize(uint32_t width, uint32_t height);
     static void BeginScene(OrthographicCamera& camera);
     static void EndScene();
     static void Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray,
