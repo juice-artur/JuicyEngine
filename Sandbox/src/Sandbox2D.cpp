8c27ba35269e9af7e4ba0b100170b54fb2c951b1
juice_artur <asasin.lol10@gmail.com>
2024-11-16 21:22:48 +0200

    feat: implement stats

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -14,6 +14,7 @@ void Sandbox2D::OnUpdate(JuicyEngine::Timestep ts)
 {
     // Update
     m_CameraController.OnUpdate(ts);
+    JuicyEngine::Renderer2D::ResetStats();
     static float rotation = 0.0f;
     rotation += ts * 50.0f;
     // Render
@@ -27,14 +28,31 @@ void Sandbox2D::OnUpdate(JuicyEngine::Timestep ts)
         JuicyEngine::Renderer2D::DrawRotatedQuad({1.0f, 0.0f}, {0.8f, 0.8f}, -45.0f, {0.8f, 0.2f, 0.3f, 1.0f});
         JuicyEngine::Renderer2D::DrawQuad({-1.0f, 0.0f}, {0.8f, 0.8f}, {0.8f, 0.2f, 0.3f, 1.0f});
         JuicyEngine::Renderer2D::DrawQuad({0.5f, -0.5f}, {0.5f, 0.75f}, {0.2f, 0.3f, 0.8f, 1.0f});
-        JuicyEngine::Renderer2D::DrawQuad({0.0f, 0.0f, -0.1f}, {10.0f, 10.0f}, m_CheckerboardTexture, 10.0f);
+        JuicyEngine::Renderer2D::DrawQuad({0.0f, 0.0f, -0.1f}, {20.0f, 20.0f}, m_CheckerboardTexture, 10.0f);
         JuicyEngine::Renderer2D::DrawRotatedQuad({-2.0f, 0.0f, 0.0f}, {1.0f, 1.0f}, rotation, m_CheckerboardTexture, 20.0f);
+        JuicyEngine::Renderer2D::EndScene();
+
+        JuicyEngine::Renderer2D::BeginScene(m_CameraController.GetCamera());
+        for (float y = -5.0f; y < 5.0f; y += 0.5f)
+        {
+            for (float x = -5.0f; x < 5.0f; x += 0.5f)
+            {
+                glm::vec4 color = {(x + 5.0f) / 10.0f, 0.4f, (y + 5.0f) / 10.0f, 0.7f};
+                JuicyEngine::Renderer2D::DrawQuad({x, y}, {0.45f, 0.45f}, color);
+            }
+        }
         JuicyEngine::Renderer2D::EndScene();
     }
 }
 void Sandbox2D::OnImGuiRender()
 {
     ImGui::Begin("Settings");
+    auto stats = JuicyEngine::Renderer2D::GetStats();
+    ImGui::Text("Renderer2D Stats:");
+    ImGui::Text("Draw Calls: %d", stats.DrawCalls);
+    ImGui::Text("Quads: %d", stats.QuadCount);
+    ImGui::Text("Vertices: %d", stats.GetTotalVertexCount());
+    ImGui::Text("Indices: %d", stats.GetTotalIndexCount());
     ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
     ImGui::End();
 }
