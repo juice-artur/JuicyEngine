67a6d6ca2d9eaa8378985851b925ba2a91cdb5e6
juice_artur <asasin.lol10@gmail.com>
2024-11-19 22:36:15 +0200

    fix: fixed editor black flicker when resize the viewport

--- JuicyEngine/src/JuicyEngine/Core/Application.h
+++ JuicyEngine/src/JuicyEngine/Core/Application.h
@@ -20,6 +20,7 @@ public:
     void PushOverlay(Layer* layer);
     inline Window& GetWindow() { return *m_Window; }
     void Close();
+    ImGuiLayer* GetImGuiLayer() { return m_ImGuiLayer; }
     inline static Application& Get() { return *s_Instance; }
 
 private:
--- JuicyEngine/src/JuicyEngine/ImGui/ImGuiLayer.h
+++ JuicyEngine/src/JuicyEngine/ImGui/ImGuiLayer.h
@@ -15,8 +15,10 @@ public:
     virtual void OnEvent(Event& e) override;
     void Begin();
     void End();
+    void BlockEvents(bool block) { m_BlockEvents = block; }
 
 private:
+    bool m_BlockEvents = true;
     float m_Time = 0.0f;
 };
 }  // namespace JuicyEngine
