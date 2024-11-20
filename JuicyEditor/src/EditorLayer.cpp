659eeef75fb268f9c4b3d2b6c948b8c247ffbf31
juice_artur <asasin.lol10@gmail.com>
2024-11-20 19:45:46 +0200

    refactor: handle resizing window in OnUpdate

--- JuicyEditor/src/EditorLayer.cpp
+++ JuicyEditor/src/EditorLayer.cpp
@@ -29,6 +29,14 @@ void EditorLayer::OnDetach() {}
 
 void EditorLayer::OnUpdate(Timestep ts)
 {
+    // Resize
+    if (JuicyEngine::FramebufferSpecification spec = m_Framebuffer->GetSpecification();
+        m_ViewportSize.x > 0.0f && m_ViewportSize.y > 0.0f &&  // zero sized framebuffer is invalid
+        (spec.Width != m_ViewportSize.x || spec.Height != m_ViewportSize.y))
+    {
+        m_Framebuffer->Resize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
+        m_CameraController.OnResize(m_ViewportSize.x, m_ViewportSize.y);
+    }
     // Update
     if (m_ViewportFocused) m_CameraController.OnUpdate(ts);
 
@@ -131,13 +139,7 @@ void EditorLayer::OnImGuiRender()
     Application::Get().GetImGuiLayer()->BlockEvents(!m_ViewportFocused || !m_ViewportHovered);
 
     ImVec2 viewportPanelSize = ImGui::GetContentRegionAvail();
-    if (m_ViewportSize != *((glm::vec2*)&viewportPanelSize) && viewportPanelSize.x > 0 && viewportPanelSize.y > 0)
-    {
-        m_Framebuffer->Resize((uint32_t)viewportPanelSize.x, (uint32_t)viewportPanelSize.y);
-        m_ViewportSize = {viewportPanelSize.x, viewportPanelSize.y};
-
-        m_CameraController.OnResize(viewportPanelSize.x, viewportPanelSize.y);
-    }
+    m_ViewportSize = {viewportPanelSize.x, viewportPanelSize.y};
     uint32_t textureID = m_Framebuffer->GetColorAttachmentRendererID();
     ImGui::Image((void*)textureID, ImVec2{m_ViewportSize.x, m_ViewportSize.y}, ImVec2{0, 1}, ImVec2{1, 0});
     ImGui::End();
