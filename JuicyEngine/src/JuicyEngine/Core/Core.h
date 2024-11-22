28ee85d1e7de58d7e15cf1b1088af8c85ad716e4
juice_artur <asasin.lol10@gmail.com>
2024-11-22 21:49:49 +0200

    refactor: improve usage JE_BIND_EVENT_FN

--- JuicyEngine/src/JuicyEngine/Core/Core.h
+++ JuicyEngine/src/JuicyEngine/Core/Core.h
@@ -77,7 +77,7 @@
 
 #define BIT(x) (1 << x)
 
-#define JE_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+#define JE_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
 
 namespace JuicyEngine
 {
