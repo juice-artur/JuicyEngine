27f3184c93f7ee6f933209a2b40b475ad2ed7587
juice-artur <asasin.lol10@gmail.com>
2024-12-18 23:50:30 +0200

    refactor: Refactored framebuffers to allow for MRTs

--- JuicyEngine/src/JuicyEngine/Renderer/Framebuffer.h
+++ JuicyEngine/src/JuicyEngine/Renderer/Framebuffer.h
@@ -2,10 +2,33 @@
 #include "JuicyEngine/Core/Core.h"
 namespace JuicyEngine
 {
+enum class FramebufferTextureFormat
+{
+    None = 0,
+    // Color
+    RGBA8,
+    // Depth/stencil
+    DEPTH24STENCIL8,
+    // Defaults
+    Depth = DEPTH24STENCIL8
+};
+struct FramebufferTextureSpecification
+{
+    FramebufferTextureSpecification() = default;
+    FramebufferTextureSpecification(FramebufferTextureFormat format) : TextureFormat(format) {}
+    FramebufferTextureFormat TextureFormat = FramebufferTextureFormat::None;
+    // TODO: filtering/wrap
+};
+struct FramebufferAttachmentSpecification
+{
+    FramebufferAttachmentSpecification() = default;
+    FramebufferAttachmentSpecification(std::initializer_list<FramebufferTextureSpecification> attachments) : Attachments(attachments) {}
+    std::vector<FramebufferTextureSpecification> Attachments;
+};
 struct FramebufferSpecification
 {
     uint32_t Width, Height;
-    // FramebufferFormat Format =
+    FramebufferAttachmentSpecification Attachments;
     uint32_t Samples = 1;
     bool SwapChainTarget = false;
 };
@@ -15,7 +38,7 @@ public:
     virtual void Bind() = 0;
     virtual void Unbind() = 0;
     virtual void Resize(uint32_t width, uint32_t height) = 0;
-    virtual uint32_t GetColorAttachmentRendererID() const = 0;
+    virtual uint32_t GetColorAttachmentRendererID(uint32_t index = 0) const = 0;
     virtual const FramebufferSpecification& GetSpecification() const = 0;
     static Ref<Framebuffer> Create(const FramebufferSpecification& spec);
 };
