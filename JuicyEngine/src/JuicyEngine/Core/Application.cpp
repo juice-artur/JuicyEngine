849f8d42f94cab8da6a71ad36f492900d3dfe57c
juice-artur <asasin.lol10@gmail.com>
2024-12-18 21:59:07 +0200

    feat: maintenance - Use `Scope` instead `std::unique_ptr` in `Application.cpp` - File dialogs start from current working directory

--- JuicyEngine/src/JuicyEngine/Core/Application.cpp
+++ JuicyEngine/src/JuicyEngine/Core/Application.cpp
@@ -17,7 +17,7 @@ Application::Application(const std::string& name)
 {
     JE_CORE_ASSERT(!s_Instance, "Application already exists!");
     s_Instance = this;
-    m_Window = std::unique_ptr<Window>(Window::Create(WindowProps(name)));
+    m_Window = Scope<Window>(Window::Create(WindowProps(name)));
     m_Window->SetEventCallback(JE_BIND_EVENT_FN(Application::OnEvent));
     Renderer::Init();
     m_ImGuiLayer = new ImGuiLayer();
