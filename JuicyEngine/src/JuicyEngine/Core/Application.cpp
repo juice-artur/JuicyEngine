3318ef700c8f793a269ed7449716afb4070b4edf
juice-artur <asasin.lol10@gmail.com>
2025-01-04 21:00:15 +0200

    feat: implement event

--- JuicyEngine/src/JuicyEngine/Core/Application.cpp
+++ JuicyEngine/src/JuicyEngine/Core/Application.cpp
@@ -1,10 +1,21 @@
 #include "Application.h"
+
+#include <JuicyEngine/Events/ApplicationEvent.h>
+
 #include <iostream>
+
+#include "Log.h"
 
 namespace JuicyEngine {
 Application::Application() {}
 Application::~Application() {}
-void Application::Run() 
-{
+void Application::Run() {
+  WindowResizeEvent e(1280, 720);
+  if (e.IsInCategory(EventCategoryApplication)) {
+    JE_TRACE(e.ToString());
+  }
+  if (e.IsInCategory(EventCategoryInput)) {
+    JE_TRACE(e.ToString());
+  }
 }
 }  // namespace JuicyEngine
