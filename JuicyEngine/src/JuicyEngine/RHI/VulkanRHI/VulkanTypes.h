25b10bd964e86e65adcaea1c95d1c79eb4e137be
juice-artur <asasin.lol10@gmail.com>
2025-01-09 22:49:53 +0200

    fix: added name space

--- JuicyEngine/src/JuicyEngine/RHI/VulkanRHI/VulkanTypes.h
+++ JuicyEngine/src/JuicyEngine/RHI/VulkanRHI/VulkanTypes.h
@@ -1,12 +1,14 @@
 #pragma once
+
 #include <vulkan/vulkan_core.h>
 #include <vector>
 
-
-
+namespace JuicyEngine
+{
 struct SwapChainSupportDetails
 {
     VkSurfaceCapabilitiesKHR capabilities;
     std::vector<VkSurfaceFormatKHR> formats;
     std::vector<VkPresentModeKHR> presentModes;
 };
+}  // namespace JuicyEngine
