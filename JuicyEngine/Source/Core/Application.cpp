fbd12e33aee6d876fde672d8b6d0f9696c5a3714
juice-artur <asasin.lol10@gmail.com>
2025-10-09 00:30:43 +0300

    feat: show window

--- JuicyEngine/Source/Core/Application.cpp
+++ JuicyEngine/Source/Core/Application.cpp
@@ -1,6 +1,17 @@
 #include <print>
+#include "SDL3/SDL.h"
 
 void SayHello()
 {
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
+        std::print("Failed init SDL");
+    }
+
+    SDL_Window* Window = SDL_CreateWindow("JuicyEngine",
+        320, 240, SDL_WINDOW_RESIZABLE | SDL_WINDOW_BORDERLESS);
+    
+    SDL_Delay(5000);
+    SDL_Quit();
     std::print("{2} {1}{0}!\n", 23, "C++", "Hello");
 }
