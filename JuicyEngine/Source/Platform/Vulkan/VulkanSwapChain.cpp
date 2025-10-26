8e76eff165137322fa9c4a8136a1efd30cbf2a3e
juice-artur <asasin.lol10@gmail.com>
2025-10-26 11:27:18 +0200

    create image views

--- JuicyEngine/Source/Platform/Vulkan/VulkanSwapChain.cpp
+++ JuicyEngine/Source/Platform/Vulkan/VulkanSwapChain.cpp
@@ -14,6 +14,9 @@ VulkanSwapChain::VulkanSwapChain()
 
 void VulkanSwapChain::Shutdown(VkDevice Device)
 {
+    for (auto ImageView : SwapChainImageViews) {
+        vkDestroyImageView(Device, ImageView, nullptr);
+    }
     vkDestroySwapchainKHR(Device, SwapChain, nullptr);
 }
 
@@ -27,7 +30,8 @@ void VulkanSwapChain::Init(VkPhysicalDevice PhysicalDevice, VkDevice Device, VkS
 
     ImageCount = SwapChainSupport.Capabilities.minImageCount + 1;
 
-    if (SwapChainSupport.Capabilities.maxImageCount > 0 && ImageCount > SwapChainSupport.Capabilities.maxImageCount) {
+    if (SwapChainSupport.Capabilities.maxImageCount > 0 && ImageCount > SwapChainSupport.Capabilities.maxImageCount)
+    {
         ImageCount = SwapChainSupport.Capabilities.maxImageCount;
     }
 
@@ -45,11 +49,14 @@ void VulkanSwapChain::Init(VkPhysicalDevice PhysicalDevice, VkDevice Device, VkS
     QueueFamilyIndices Indices = VulkanDevice::FindQueueFamilies(PhysicalDevice, Surface);
     uint32_t queueFamilyIndices[] = {Indices.GraphicsFamily.value(), Indices.GraphicsFamily.value()};
 
-    if (Indices.GraphicsFamily != Indices.GraphicsFamily) {
+    if (Indices.GraphicsFamily != Indices.GraphicsFamily)
+    {
         SwapchainCreateInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
         SwapchainCreateInfo.queueFamilyIndexCount = 2;
         SwapchainCreateInfo.pQueueFamilyIndices = queueFamilyIndices;
-    } else {
+    }
+    else
+    {
         SwapchainCreateInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
     }
 
@@ -69,6 +76,8 @@ void VulkanSwapChain::Init(VkPhysicalDevice PhysicalDevice, VkDevice Device, VkS
 
     SwapChainImageFormat = SurfaceFormat.format;
     SwapChainExtent = Extent;
+
+    CreateImageViews(Device);
 }
 
 SwapChainSupportDetails VulkanSwapChain::QuerySwapChainSupport(VkPhysicalDevice Device, VkSurfaceKHR Surface)
@@ -79,15 +88,17 @@ SwapChainSupportDetails VulkanSwapChain::QuerySwapChainSupport(VkPhysicalDevice 
     uint32_t FormatCount;
     vkGetPhysicalDeviceSurfaceFormatsKHR(Device, Surface, &FormatCount, nullptr);
 
-    if (FormatCount != 0) {
+    if (FormatCount != 0)
+    {
         Details.Formats.resize(FormatCount);
         vkGetPhysicalDeviceSurfaceFormatsKHR(Device, Surface, &FormatCount, Details.Formats.data());
     }
-    
+
     uint32_t PresentModeCount;
     vkGetPhysicalDeviceSurfacePresentModesKHR(Device, Surface, &PresentModeCount, nullptr);
 
-    if (PresentModeCount != 0) {
+    if (PresentModeCount != 0)
+    {
         Details.PresentModes.resize(PresentModeCount);
         vkGetPhysicalDeviceSurfacePresentModesKHR(Device, Surface, &PresentModeCount, Details.PresentModes.data());
     }
@@ -96,8 +107,10 @@ SwapChainSupportDetails VulkanSwapChain::QuerySwapChainSupport(VkPhysicalDevice 
 
 VkSurfaceFormatKHR VulkanSwapChain::ChooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& AvailableFormats)
 {
-    for (const auto& AvailableFormat : AvailableFormats) {
-        if (AvailableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && AvailableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+    for (const auto& AvailableFormat : AvailableFormats)
+    {
+        if (AvailableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && AvailableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
+        {
             return AvailableFormat;
         }
     }
@@ -106,8 +119,10 @@ VkSurfaceFormatKHR VulkanSwapChain::ChooseSwapSurfaceFormat(const std::vector<Vk
 
 VkPresentModeKHR VulkanSwapChain::ChooseSwapPresentMode(const std::vector<VkPresentModeKHR>& AvailablePresentModes)
 {
-    for (const auto& AvailablePresentMode : AvailablePresentModes) {
-        if (AvailablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
+    for (const auto& AvailablePresentMode : AvailablePresentModes)
+    {
+        if (AvailablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR)
+        {
             return AvailablePresentMode;
         }
     }
@@ -138,5 +153,29 @@ VkExtent2D VulkanSwapChain::ChooseSwapExtent(const VkSurfaceCapabilitiesKHR& Cap
         return ActualExtent;
     }
 }
-}
 
+void VulkanSwapChain::CreateImageViews(const VkDevice Device)
+{
+    SwapChainImageViews.resize(SwapChainImages.size());
+    for (size_t i = 0; i < SwapChainImages.size(); i++)
+    {
+        VkImageViewCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+        createInfo.image = SwapChainImages[i];
+        createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+        createInfo.format = SwapChainImageFormat;
+        createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+        createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+        createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+        createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+        createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        createInfo.subresourceRange.baseMipLevel = 0;
+        createInfo.subresourceRange.levelCount = 1;
+        createInfo.subresourceRange.baseArrayLayer = 0;
+        createInfo.subresourceRange.layerCount = 1;
+
+        auto Result = vkCreateImageView(Device, &createInfo, nullptr, &SwapChainImageViews[i]);
+        JE_CORE_ASSERT(Result == VK_SUCCESS, "Failed to create image views!");
+    }
+}
+}
--- JuicyEngine/Source/Platform/Vulkan/VulkanSwapChain.h
+++ JuicyEngine/Source/Platform/Vulkan/VulkanSwapChain.h
@@ -22,12 +22,16 @@ public:
     VkSurfaceFormatKHR ChooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& AvailableFormats);
     VkPresentModeKHR ChooseSwapPresentMode(const std::vector<VkPresentModeKHR>& AvailablePresentModes);
     VkExtent2D ChooseSwapExtent(const VkSurfaceCapabilitiesKHR& Capabilities, void* Window);
+
+private:
+    void CreateImageViews(const VkDevice Device);
 private:
     VkSwapchainKHR SwapChain;
     uint32_t ImageCount;
     std::vector<VkImage> SwapChainImages;
     VkFormat SwapChainImageFormat;
     VkExtent2D SwapChainExtent;
+    std::vector<VkImageView> SwapChainImageViews;
 };
 }
 
